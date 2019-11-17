c848f04d8e844da995e48ca6eac603a88bab8e6c
Kfriedman <Koryf91@gmail.com>
2019-11-16 23:41:27 -0800

    got the api working properly. added some buttons to start

--- assest/javascript/script.js
+++ assest/javascript/script.js
@@ -1,1 +1,34 @@
 console.log("Testing testing is this thing on?")
+
+$("button").on("click", function () {
+
+  var animal = $(this).attr("data-animal");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=VK1xaOZy28bs02RHNjqCEzsF3rkKPUsN&limit=10";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(queryURL);
+    console.log(response);
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var animals = $("<div>");
+      var p = $("<p>").text("Rating : " + results[i].rating);
+      var animalImg = $("<img>");
+      animalImg.attr("src", results[i].images.fixed_height.url);
+      animals.append(p);
+      animals.append(animalImg);
+      $("#gifs-here").prepend(animals);
+    }
+
+  });
+
+});
+
+
+
