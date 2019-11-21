de0ca464027a6be74e95dc2ce5bfd5fa6f7d0dae
Kfriedman <Koryf91@gmail.com>
2019-11-21 00:06:40 -0800

    got my search to work

--- assest/javascript/script.js
+++ assest/javascript/script.js
@@ -21,8 +21,10 @@ $("button").on("click", function () {
       var p = $("<p>").text("Rating : " + results[i].rating);
       var animalImg = $("<img>");
       animalImg.attr("src", results[i].images.fixed_height.url);
+     
       animals.append(p);
       animals.append(animalImg);
+
       $("#gifs-here").prepend(animals);
     }
 
@@ -30,5 +32,40 @@ $("button").on("click", function () {
 
 });
 
+var input = $("#search");
+var submitt = $("#add");
+
+$("#add").on("click", function (event) {
+  event.preventDefault();
+
+  var inputVal = input.val();
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + inputVal + "&api_key=VK1xaOZy28bs02RHNjqCEzsF3rkKPUsN&limit=10";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(queryURL);
+    console.log(response);
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var animals = $("<div>");
+      var p = $("<p>").text("Rating : " + results[i].rating);
+      var animalImg = $("<img>");
+      animalImg.attr("src", results[i].images.fixed_height.url);
+     
+      animals.append(p);
+      animals.append(animalImg);
+      $("#buttons-view").prepend("<button>");
+      $("#gifs-here").prepend(animals);
+    }
+
+  });
+
+});
 
 
